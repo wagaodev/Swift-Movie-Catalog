dcc322611ff0d81ddd3408e3ad6afce1826abb93
Wagão - ( Wagner Goulart ) <wagner.goulart@neon.com.br>
2022-07-12 17:21:48 -0300

    Initial Commit

---
+++ movieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieList
+//
+//  Created by Wagner Barboza Goulart on 12/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
